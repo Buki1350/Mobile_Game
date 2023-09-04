9099a63d07af29bd24f9acd6c1ba271b687eda8f
EKHO <lukasz.ksiazek1350@gmail.com>
2023-09-04 14:13:05 +0200

    Reload

---
+++ Mobile_Game/Assets/Scipts/LevelGeneration.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGeneration : MonoBehaviour
+{
+    [SerializeField] GameObject Player;
+    GameObject[] RoomX;
+    GameObject[] RoomT;
+    GameObject[] RoomL;
+    GameObject[] RoomI;
+    GameObject[] Room1;
+
+    
+
+
+    void Start()
+    {
+    }
+
+    private void Update()
+    {
+        
+    }
+
+}
