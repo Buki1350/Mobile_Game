cd24b1ef10d17de57df443680a1fe8596b4be88b
EKHO <lukasz.ksiazek1350@gmail.com>
2024-02-26 17:49:01 +0100

    Player Movement/Weapons/Animations scripts
    Player script rebuild and updated

---
+++ Mobile_Game/Assets/Scipts/Player/PlayerAnimations.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimations : MonoBehaviour
+{
+    [SerializeField] Animator animator;
+
+    PlayerMovement pMovement;
+    PlayerWeapons pWeapons;
+    void Start()
+    {
+        if (gameObject.GetComponent<PlayerMovement>() == null)
+            Debug.LogError($"<color=red>PlayerAnimations: PlayerMovement script is missing!</color>");
+        else
+            pMovement = gameObject.GetComponent<PlayerMovement>();
+
+        if (gameObject.GetComponent<PlayerWeapons>() == null)
+            Debug.LogError($"<color=red>PlayerAnimations: PlayerWeapons script is missing!</color>");
+        else
+            pWeapons = gameObject.GetComponent<PlayerWeapons>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetFloat("WalkBlendX", pMovement.walkBlendX);
+        animator.SetFloat("WalkBlendY", pMovement.walkBlendY);
+        animator.SetBool("isFighting", pMovement.isFighting);
+        animator.SetBool("isMoving", pMovement.isMoving);
+
+        int weaponIndex = 0;
+        if (pMovement.isFighting)
+        {
+            foreach (GameObject sword in pWeapons.swords)
+                sword.SetActive(false);
+
+            foreach (GameObject greatsword in pWeapons.greatswords)
+                greatsword.SetActive(false);
+            
+            foreach (GameObject axe in pWeapons.axes)
+                axe.SetActive(false);
+
+            foreach (GameObject greataxe in pWeapons.greataxes)
+                greataxe.SetActive(false);
+
+            animator.SetLayerWeight(animator.GetLayerIndex("UpperBody"), 1);
+            switch (pWeapons.currentWeapon)
+            {
+                case Weapons.none:
+                    weaponIndex = 0;
+                    pWeapons.swords[pWeapons.currentWeaponIndex].SetActive(true);
+                    break;
+                case Weapons.sword: weaponIndex = 1; break;
+                case Weapons.greatsword: weaponIndex = 2; break;
+                case Weapons.axe: weaponIndex = 3; break;
+                case Weapons.greataxe: weaponIndex = 4; break;
+            }
+        }
+        else
+            animator.SetLayerWeight(animator.GetLayerIndex("UpperBody"), 0);
+        animator.SetInteger("weaponIndex", weaponIndex);
+
+
+    }
+}
---
+++ Mobile_Game/Assets/Scipts/Player/PlayerWeapons.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Weapons
+{
+    none,
+    sword,
+    greatsword,
+    axe,
+    greataxe
+}
+
+public class PlayerWeapons : MonoBehaviour
+{
+    [Header("Weapons on player model (in hand) ordered by its index")]
+    [SerializeField] public GameObject[] swords;
+    [SerializeField] public GameObject[] greatswords;
+    [SerializeField] public GameObject[] axes;
+    [SerializeField] public GameObject[] greataxes;
+
+    [NonSerialized] public Weapons currentWeapon = Weapons.none;
+    [NonSerialized] public int currentWeaponIndex = 0;
+    [NonSerialized] public Weapons secondaryWeapon = Weapons.none;
+    [NonSerialized] public int secondaryWeaponIndex = 0;
+}
